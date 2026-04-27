1e01faa28347c63cfcd78496252fa2dd4b6009ff
Gabe <126498679+Gabs0078@users.noreply.github.com>
2026-04-26 21:14:16 -0300

    Fix export syntax for sequelize instance

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 sequelize = new Sequelize('postgres://postgres:app@app-database:5432/escola', {dialect: 'postgres'})
 
-export const sequelize = new Sequelize(url, { dialect: 'postgres' })
+export { sequelize }; = new Sequelize(url, { dialect: 'postgres' })
   logging: false,
   define: { underscored: true },
 });
