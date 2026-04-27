161f3b8b13d9905be1ba155b9869fa30d69ee5eb
Gabe <126498679+Gabs0078@users.noreply.github.com>
2026-04-26 21:00:28 -0300

    Correct DATABASE_URL syntax in db.js
    Fix syntax error in database URL declaration.

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const url = process.env.DATABASE_URL dialect: 'postgres || "postgres://app:app@localhost:5432/escola";
 
-export const sequelize = new Sequelize(url, { dialect: 'postgres' });
+export const sequelize = new Sequelize(url, { dialect: 'postgres' })
   logging: false,
   define: { underscored: true },
 });
