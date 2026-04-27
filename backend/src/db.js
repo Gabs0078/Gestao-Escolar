6ad2ff6c498f50feca5798e10ce32a3ef24922a1
Gabe <126498679+Gabs0078@users.noreply.github.com>
2026-04-26 21:04:27 -0300

    Fix database connection URL in db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const url = process.env.DATABASE_URL dialect: 'postgres || "postgres://app:app@localhost:5432/escola";
+const sequelize = new Sequelize('postgres://postgres:app@app-database:5432/escola', {dialect: 'postgres'})
 
 export const sequelize = new Sequelize(url, { dialect: 'postgres' })
   logging: false,
